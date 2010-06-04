3d5792f320ae43cbde948f9e18b6ce2eff068fa5
r.malinowski88 <r.malinowski88@acf96ec3-e1d8-8f09-53fb-2b4d695f705e>
2010-06-04 22:28:25 +0000

    działające (chyba) udp source + drobne zmiany w mainie

--- fredsimulator/src/common/UDPSource.java
+++ fredsimulator/src/common/UDPSource.java
@@ -2,13 +2,18 @@ package common;
 
 public class UDPSource extends Node {
 
-	public UDPSource(int id) {
+	public UDPSource(int id, long bitrate) {
 		super(id);
+		modulo = (int) (Constans.udp_packet_size * 8 / bitrate * Constans.second);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void handle(long time) {
+		if(Timer.getTime()% modulo == 0){
+			UDPPacket packet = new UDPPacket(this, Constans.udp_packet_size);
+			links.first().placeInLink(packet);
+		}
 		// TODO Auto-generated method stub
 		
 	}
@@ -18,4 +23,5 @@ public class UDPSource extends Node {
 	 */
 	long bitrate;
 	
+	int modulo;
 }
