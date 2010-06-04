c78a951b976423f7152ec03ea37956b2f278a329
czarek.zawadka <czarek.zawadka@acf96ec3-e1d8-8f09-53fb-2b4d695f705e>
2010-06-04 14:10:58 +0000

    rozwiniete szkielety klas

--- fredsimulator/src/common/FREDmain.java
+++ fredsimulator/src/common/FREDmain.java
@@ -1,23 +1,33 @@
 package common;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class FREDmain {
 
 	/**
 	 * @param args
 	 */
 	
-	Timer timer;
+	static Timer timer;
 	
-	
-	//
-	//
+	static ArrayList<Node> nodes;
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		
 		while (true)
 		{
-			
+			for (Iterator<Node> iterator = nodes.iterator(); iterator.hasNext();) {
+				timer.increment();
+				long time = timer.getTime();
+				Node node = (Node) iterator.next();
+				node.handle(time);				
+			}
 			
 			
 			
