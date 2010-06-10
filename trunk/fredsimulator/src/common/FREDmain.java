f31e4368109a28fa1e4ebf247ef2365ad9a12ffa
r.malinowski88 <r.malinowski88@acf96ec3-e1d8-8f09-53fb-2b4d695f705e>
2010-06-10 08:26:27 +0000

    zaimplementowano freda, wprawdzie jest jeden exception, ale bede z tym walczyl

--- trunk/fredsimulator/src/common/FREDmain.java
+++ trunk/fredsimulator/src/common/FREDmain.java
@@ -18,22 +18,28 @@ public class FREDmain {
 		nodes = new ArrayList<Node>();
 		links = new ArrayList<Link>();
 		///tworzenie sieci testowej:
-		FakeNode fake = new FakeNode(0);
-//		Fred fred = new Fred(0, Timer.timer);
+		//FakeNode fake = new FakeNode(0);
+		Fred fred = new Fred(0);
 		UDPSource udpSource = new UDPSource(1, 5000000); 
 		TCPSource tcpSource1 = new TCPSource(2);
 		TCPSource tcpSource2 = new TCPSource(3);
 //		TCPSource tcpSource2 = new TCPSource(3);
 		Sink sink = new Sink(4);
-		nodes.add(fake);
+		
+		//nodes.add(fake);
+		nodes.add(fred);
 		nodes.add(udpSource);
 		nodes.add(tcpSource1);
 		nodes.add(tcpSource2);
 		nodes.add(sink);
-		Link link1 = new Link(udpSource, fake, (int) ((Constans.second)/100),  10 * 1000 * 1000);
-		Link link2 = new Link(fake, sink, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
-		Link link3 = new Link(tcpSource1, fake, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
-		Link link4 = new Link(tcpSource2, fake, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
+//		Link link1 = new Link(udpSource, fake, (int) ((Constans.second)/100),  10 * 1000 * 1000);
+//		Link link2 = new Link(fake, sink, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
+//		Link link3 = new Link(tcpSource1, fake, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
+//		Link link4 = new Link(tcpSource2, fake, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
+		Link link1 = new Link(udpSource, fred, (int) ((Constans.second)/100),  10 * 1000 * 1000);
+		Link link2 = new Link(fred, sink, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
+		Link link3 = new Link(tcpSource1, fred, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
+		Link link4 = new Link(tcpSource2, fred, (int) ((Constans.second)/100), 10 * 1000 * 1000 );
 		links.add(link1);
 		links.add(link2);
 		links.add(link3);
